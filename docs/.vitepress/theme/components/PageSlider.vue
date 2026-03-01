e984b72b8ba78c768750959aec9afeeb666f9264
xmbsm <21903538@qq.com>
2026-03-01 13:29:58 +0800

    第一次提交

--- docs/.vitepress/theme/components/PageSlider.vue
+++ docs/.vitepress/theme/components/PageSlider.vue
@@ -50,12 +50,12 @@
             <!-- 导航按钮 -->
             <div class="carousel-button next" @click="nextSlide">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 5L19 12L8 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M8 7L14 12L8 17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
             <div class="carousel-button prev" @click="prevSlide">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 5L8 12L19 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M14 7L8 12L14 17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
             <!-- 分页指示器 -->
