4c129763490c2cf0f32eae6573dcf8552f09c0d9
xmbsm <21903538@qq.com>
2026-03-13 15:08:21 +0800

    第一次提交

--- docs/.vitepress/theme/components/ArticleCC.vue
+++ docs/.vitepress/theme/components/ArticleCC.vue
@@ -14,8 +14,8 @@
       <li class="li"><label class="label">标题</label><span class="span">{{ frontmatter?.title }}</span></li>
       <li class="li"><label class="label">时间</label><span class="span">{{ formatTime(frontmatter?.date) }}</span></li>
       <li class="li link" @click="handleCopy(articleLink)"><label class="label">链接</label><span v-tooltip="'点击复制'" class="span">{{ articleLink }}</span></li>
-      <li class="li" v-if="theme?.article?.cc?.licenseLink"><label class="span label">版权</label><span class="span"><a class="a"
-            target="_blank" :href="theme?.article?.cc?.licenseLink">{{ theme?.article?.cc?.license }}</a></span></li>
+  <!-- <li class="li" v-if="theme?.article?.cc?.licenseLink"><label class="span label">版权</label><span class="span"><a class="a"
+            target="_blank" :href="theme?.article?.cc?.licenseLink">{{ theme?.article?.cc?.license }}</a></span></li> -->
     </ul>
   </div>
 </template>
