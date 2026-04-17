52c0fd430c01c04a24c4a18f6496bf1c033249d9
xmbsm <21903538@qq.com>
2026-04-17 16:22:16 +0800

    提交

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,7 +16,7 @@ export default defineConfig<ThemeConfig>({
             })
         }
     },
-    title: '小酷字体',
+    title: '最新最全的可免费商用中文字体下载网站！无版权免费字体下载！',
     base: '/',
     head,
     description: '最新最全的可免费商用中文字体下载网站！无版权免费字体下载！',
